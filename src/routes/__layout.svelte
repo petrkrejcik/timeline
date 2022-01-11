60ead30577f01058187275795fc1c0212e55bd11
petrkrejcik <krejcik.petr@gmail.com>
2022-01-11 07:29:11 +0000

    customize default content

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script>
+  import '../app.css';
+</script>
+
+<slot />
+
+<footer>
+  <p>
+    Created with <a class="link" rel="noopener" href="https://svelte.dev"
+      >SvelteKit</a
+    >
+    and deployed with
+    <a class="link" target="_blank" rel="noopener" href="https://vercel.com"
+      >▲ Vercel</a
+    >.
+  </p>
+</footer>
---
+++ src/routes/portfolio.svelte
@@ -0,0 +1,6 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<h1>My Portfolio page</h1>
+<p>I'm a developer and this is what I've done.</p>
