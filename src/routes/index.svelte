6db79ecb6b4fc25f4406e99ce7dede7624cd1470
petrkrejcik <krejcik.petr@gmail.com>
2022-01-11 21:59:44 +0000

    graphcms request

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,46 @@
 <script context="module">
+  import { gql, GraphQLClient } from 'graphql-request';
   export const prerender = true;
+
+  export async function load() {
+    const graphcms = new GraphQLClient(import.meta.env.VITE_GRAPHCMS_URL, {
+      headers: {}
+    });
+
+    const query = gql`
+      query PostsIndex {
+        posts {
+          id
+          title
+          slug
+          date
+          excerpt
+        }
+      }
+    `;
+
+    const { posts } = await graphcms.request(query);
+
+    return {
+      props: {
+        posts
+      }
+    };
+  }
+</script>
+
+<script>
+  export let posts;
 </script>
 
 <h1>Hi, I'm Petr</h1>
 <p>I'm a developer and this is what I've done.</p>
 <ul>
   <li>
-    <a class="card-link" rel="noopener" href="/portfolio">
-      <h2>Portfolio</h2>
-      <p>{new Date().toLocaleString()}</p>
-    </a>
+    {#each posts as post}
+      <li>
+        <a href="/post/{post.slug}">{post.title}</a>
+      </li>
+    {/each}
   </li>
 </ul>
